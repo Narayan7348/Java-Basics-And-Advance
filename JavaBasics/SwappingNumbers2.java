68ebdbf513f94fdf40732d706a331eef12f6239a
Narayan <narayanthakur7348@gmail.com>
2025-08-16 15:56:04 +0530

    Added program for SwappingNumbers without using third variable-JavaBasics

---
+++ JavaBasics/SwappingNumbers2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class SwappingNumbers2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+        a = a + b; 
+        b = a - b;
+        a = a - b;
+        System.out.println("After swapping: ");
+        System.out.println("First number: " + a);   
+        System.out.println("Second number: " + b);
+        
+
+    }
+}
