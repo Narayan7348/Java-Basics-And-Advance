3b6358ff2ffd08b41dd7c5728d35746f80d3e402
Narayan <narayanthakur7348@gmail.com>
2025-08-20 14:31:36 +0530

    Added program for to check a person eligible to vote  or not  using if else statement-JavaBasics

---
+++ JavaBasics/VoteEligibility.java
@@ -0,0 +1,13 @@
+import java.util.*; // Program to check if a person is eligible to vote
+public class VoteEligibility {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
