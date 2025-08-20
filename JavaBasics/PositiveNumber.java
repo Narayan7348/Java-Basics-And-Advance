28ca44c8d33002448d70180f709c9e91fb0668b1
Narayan <narayanthakur7348@gmail.com>
2025-08-20 14:44:22 +0530

    Added program for to check a Number Positive  or not  using if statement-JavaBasics

---
+++ JavaBasics/PositiveNumber.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PositiveNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if(number>0){
+            System.out.println(number + " is a positive number.");
+        } 
+        
+    }
+    
+}
