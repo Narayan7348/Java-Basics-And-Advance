fff2ec2d4453fcf8f848e480b57f31a26f2b8870
Narayan <narayanthakur7348@gmail.com>
2025-08-21 15:45:18 +0530

    Added program for to print Number from 1 to 10  using for Loop statement-JavaBasics

---
+++ JavaBasics/PrintNumber1To10.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class PrintNumber1To10 {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            System.out.print(i+" ");
+        }
+    }
+    
+}
