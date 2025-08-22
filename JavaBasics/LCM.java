014056448bdcc288eb4e5a2d46e9b1bfc4f58839
Narayan <narayanthakur7348@gmail.com>
2025-08-22 13:55:57 +0530

    Added program for LCM-JavaBasics

---
+++ JavaBasics/LCM.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class LCM {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b=sc.nextInt();
+        int lcm=a;
+        if (b > a) {
+            lcm = b; // Start with the larger number
+        }   
+        while(true){
+            if(lcm % a == 0 && lcm % b == 0){
+                System.out.println("LCM of " + a + " and " + b + " is: " + lcm);
+                break;
+            }
+            lcm++;
+        }
+    }
+    
+}
