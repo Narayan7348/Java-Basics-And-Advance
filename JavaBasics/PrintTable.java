423f51bb3d878e1e89dc2e911e1ea55adc20ab35
Narayan <narayanthakur7348@gmail.com>
2025-08-22 14:02:09 +0530

    Added program for printing table of a given number, natural number sum, and even numbers

--- JavaBasics/PrintTable.java
+++ JavaBasics/PrintTable.java
@@ -4,7 +4,8 @@ public class PrintTable {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter a number to print its multiplication table: ");
         int num = sc.nextInt();
-        int result = 0;
+        int result ;
+        
         System.out.println("Table of given number is:");
         for(int i=1;i<=10;i++){
             result = num * i;
